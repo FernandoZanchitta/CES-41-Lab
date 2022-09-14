5df5ddd105beab43d628a37c723b61069a688e32
FernandoZanchitta <50930741+FernandoZanchitta@users.noreply.github.com>
2022-09-14 10:29:56 -0300

    Editando comentarios iniciais dos arquivos

--- lab1/globals.h
+++ lab1/globals.h
@@ -1,9 +1,7 @@
 /****************************************************/
 /* File: globals.h                                  */
-/* Global types and vars for TINY compiler          */
+/* Global types and vars for C- compiler          */
 /* must come before other include files             */
-/* Compiler Construction: Principles and Practice   */
-/* Kenneth C. Louden                                */
 /****************************************************/
 
 #ifndef _GLOBALS_H_
--- lab1/readme.txt
+++ lab1/readme.txt
@@ -6,7 +6,7 @@ $ gcc -o lexical *.o -lfl
 
 (Em Mac pode ser que funcione com -ll no lugar de -lfl)
 
-Executavel gerado:
+Executando testes:
 
 $ ./lexical test/teste_ordMatrix.c
 $ ./lexical test/teste_sortArrays.c 
