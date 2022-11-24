8bea3f87cda51f6ad9a827c1afde9278fa5323f9
mateusnobre <mateusnobrests@gmail.com>
2022-11-23 21:34:34 -0300

    commit

--- lab3/test/teste_example.c
+++ lab3/test/teste_example.c
@@ -1,8 +1,14 @@
 void main(int a)
 {
-/*adeadeadaee*/
-    a = 1*2;
+/*adeadeadaee
     int b;
-    b = 8 + a;
+    a = 1*2;
+    b = 8 + a;*/
+    if (3 == a){
+        a = 2;
+    }
+    else {
+        a = 5;
+    }
 /* dadaedea */
 } 
